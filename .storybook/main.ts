c7970466731beda750d8f26f7c533ab415a92c03
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-14 03:12:56 +0900

    🧰  storybookでsvgコンポーネントを使えるようにした

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs'
+import { svgrWebpackConfig } from '../svgr.webpack.config'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -16,7 +17,20 @@ const config: StorybookConfig = {
   ],
   framework: {
     name: '@storybook/nextjs',
-    options: {},
+    options: { fastRefresh: true },
+  },
+  webpackFinal: (config) => {
+    // Default rule for images /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test && rule.test.test('.svg'))
+    fileLoaderRule.exclude = /\.svg$/
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: 'pre',
+      loader: require.resolve('@svgr/webpack'),
+    })
+
+    return config
   },
   docs: {
     autodocs: true,
