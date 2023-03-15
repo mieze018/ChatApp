cd74e95b627c18abbe26ffa4a155a843df642386
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-15 23:24:18 +0900

    Update LayoutSignIn.tsx

--- src/components/layout/LayoutSignIn.tsx
+++ src/components/layout/LayoutSignIn.tsx
@@ -3,18 +3,24 @@ import tw from 'twin.macro'
 import type { LayoutProps } from '@/src/components/layout/Layout'
 import type { LayoutChat } from '@/src/components/layout/LayoutChat'
 import type { useGetMessagesType } from '@/src/hooks/firebase/useGetMessages'
-import type { useSendMessageType } from '@/src/hooks/firebase/useSendMessage'
+import type { useSignUpType } from '@/src/hooks/firebase/useSingUp'
 import type { authUserType, chatType } from '@/src/types/firebaseDB'
 
-import { ChatInput } from '@/src/components/combined/ChatInput'
-import { ChatMessageList } from '@/src/components/combined/ChatMessageList'
+import NoUserImageIcon from '@/public/icons/no-user-image.svg'
+import { Button } from '@/src/components/atom/Button'
+import { ErrorWrapper } from '@/src/components/atom/ErrorWrapper'
+import { InputFile } from '@/src/components/atom/InputFIle'
+import { TextInput } from '@/src/components/atom/TextInput'
 import { Layout } from '@/src/components/layout/Layout'
+import { microCopies } from '@/src/libs/microCopies'
 
 const WrapperSignIn = tw.div`h-full w-full 
 grid items-center justify-center
 px-4 mx-auto
 `
-const ScrollWrapper = tw.div`overflow-y-auto w-screen h-[inherit]`
+const Card = tw.div`grid gap-8 items-center justify-center p-10 bg-white rounded shadow-md max-w-sm mx-auto`
+const Label = tw.label`grid justify-items-center`
+const ButtonWrapper = tw.div`grid items-center gap-1 justify-items-center`
 
 export const LayoutSignIn: React.FC<
   LayoutProps & {
@@ -22,17 +28,59 @@ export const LayoutSignIn: React.FC<
     chats: chatType[]
     isLoading: useGetMessagesType['isLoading']
     isBlank: useGetMessagesType['isBlank']
-    message: chatType['message']
-    setMessage: useSendMessageType['setMessage']
-    handleSendMessage: useSendMessageType['handleSendMessage']
+    handleSignUp: useSignUpType['handleSignUp']
+    setDisplayName: useSignUpType['setDisplayName']
+    displayName: useSignUpType['displayName']
+    setFile: useSignUpType['setFile']
+    file: useSignUpType['file']
+    error: useSignUpType['error']
+    progress: useSignUpType['progress']
+    isSubmitBlocked: boolean
   }
-> = ({ user, onLogout, chats, isLoading, isBlank, message, setMessage, handleSendMessage }) => (
+> = ({
+  user,
+  onLogout,
+  handleSignUp,
+  setDisplayName,
+  displayName,
+  setFile,
+  error,
+  progress,
+  isSubmitBlocked,
+}) => (
   <Layout user={user} onLogout={onLogout} isOverflowYHidden={true}>
     <WrapperSignIn>
-      <ScrollWrapper>
-        <ChatMessageList chats={chats} user={user} isLoading={isLoading} isBlank={isBlank} />
-      </ScrollWrapper>
-      <ChatInput message={message} setMessage={setMessage} handleSendMessage={handleSendMessage} />
+      <form onSubmit={handleSignUp}>
+        <Card>
+          <p className="my-8 text-2xl font-semibold"> {microCopies.チャットを始めましょう}</p>
+
+          <Label css={tw`cursor-pointer`}>
+            <NoUserImageIcon css={tw`w-20 h-20 mb-2 text-gray-light`} />
+
+            <InputFile accept="image/*" onChange={(e) => setFile(e.target.files?.[0])} />
+            <span className="sr-only t-2">{microCopies.ユーザーアイコンを選択してください}</span>
+          </Label>
+
+          <Label>
+            <TextInput
+              type="text"
+              value={displayName}
+              onChange={(e) => setDisplayName(e.target.value)}
+              required
+              className="p-3 mb-5 border-2 rounded outline-none w-80 focus:border-purple-700"
+              placeholder={microCopies.お名前}
+            />
+          </Label>
+          <ButtonWrapper>
+            <Button type="submit" disabled={isSubmitBlocked}>
+              {microCopies.チャットルームへ入室}
+            </Button>
+            <progress value={progress} max="100"></progress>
+          </ButtonWrapper>
+
+          {error && <ErrorWrapper>{error.message}</ErrorWrapper>}
+        </Card>
+      </form>
     </WrapperSignIn>
   </Layout>
 )
