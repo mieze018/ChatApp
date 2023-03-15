2f99d6ba30b9e52eb8a7f296429307dd37c5260a
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-15 23:53:50 +0900

    Update microCopies.ts

--- src/libs/microCopies.ts
+++ src/libs/microCopies.ts
@@ -2,7 +2,7 @@ export const microCopies = {
   signInHeader: 'チャットを始めましょう',
   signInSubmit: 'チャットルームへ入室',
   srAvatarInput: 'ユーザーアイコンを選択してください',
-  お名前: 'お名前',
+  signInDisplayNamePlaceholder: 'ユーザー名',
   chatBlank: '初めてのチャットを投稿しましょう',
   noName: 'No Name',
 }
