a7456d6f123a3aec139b27a37ef539830b71033f
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-15 22:51:11 +0900

    レイアウト調整

--- src/components/layout/Layout.stories.tsx
+++ src/components/layout/Layout.stories.tsx
@@ -3,7 +3,7 @@ import { within, userEvent } from '@storybook/testing-library'
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { Layout } from '@/src/components/layout/Layout'
-import { mockUser } from '@/src/libs/mock'
+import { mockSentenceLongEnglish, mockUser } from '@/src/libs/mock'
 
 const meta: Meta<typeof Layout> = {
   component: Layout,
@@ -12,61 +12,11 @@ const meta: Meta<typeof Layout> = {
 
 export default meta
 type Story = StoryObj<typeof meta>
-const Contents = () => (
-  <div className="px-5 py-12">
-    <h2>Pages in Storybook</h2>
-    <p>
-      We recommend building UIs with a
-      <a href="https://componentdriven.org" target="_blank" rel="noopener noreferrer">
-        <strong>component-driven</strong>
-      </a>
-      process starting with atomic components and ending with pages.
-    </p>
-    <p>
-      Render pages with mock data. This makes it easy to build and review page states without
-      needing to navigate to them in your app. Here are some handy patterns for managing page data
-      in Storybook:
-    </p>
-    <ul>
-      <li>
-        Use a higher-level connected component. Storybook helps you compose such data from the
-        &quot;args&quot; of child component stories
-      </li>
-      <li>
-        Assemble data in the page component from your services. You can mock these services out
-        using Storybook.
-      </li>
-    </ul>
-    <p>
-      Get a guided tutorial on component-driven development at{' '}
-      <a href="https://storybook.js.org/tutorials/" target="_blank" rel="noopener noreferrer">
-        Storybook tutorials
-      </a>
-      . Read more in the{' '}
-      <a href="https://storybook.js.org/docs" target="_blank" rel="noopener noreferrer">
-        docs
-      </a>
-      .
-    </p>
-    <div className="tip-wrapper">
-      <span className="tip">Tip</span> Adjust the width of the canvas with the{' '}
-      <svg width="10" height="10" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-        <g fill="none" fillRule="evenodd">
-          <path
-            d="M1.5 5.2h4.8c.3 0 .5.2.5.4v5.1c-.1.2-.3.3-.4.3H1.4a.5.5 0 01-.5-.4V5.7c0-.3.2-.5.5-.5zm0-2.1h6.9c.3 0 .5.2.5.4v7a.5.5 0 01-1 0V4H1.5a.5.5 0 010-1zm0-2.1h9c.3 0 .5.2.5.4v9.1a.5.5 0 01-1 0V2H1.5a.5.5 0 010-1zm4.3 5.2H2V10h3.8V6.2z"
-            id="a"
-            fill="#999"
-          />
-        </g>
-      </svg>
-      Viewports addon in the toolbar
-    </div>
-  </div>
-)
+const Contents = () => <div className="px-5 py-12">{mockSentenceLongEnglish}</div>
 
 export const LoggedIn: Story = {
   args: {
-    ...mockUser,
+    user: mockUser,
     children: <Contents />,
   },
   play: async ({ canvasElement }) => {
@@ -81,12 +31,5 @@ export const LoggedOut: Story = {
   args: {
     ...LoggedIn.args,
     user: undefined,
-  },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-    const loginButton = await canvas.getByRole('button', {
-      name: /ログイン/i,
-    })
-    await userEvent.click(loginButton)
   },
 }
--- src/libs/mock.ts
+++ src/libs/mock.ts
@@ -17,6 +17,8 @@ const mockUser3 = {
     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAABBUlEQVR4nOXSzyqEURjH8Q9xARakXIHsJ8otWNixV/7UZMuCJsWsrWYv1hbchVnLBUhqsFOjMHrrTL3xDvOMdxbyq2d1fuf7recc/luOcDgs+AI6aeaHITjLCU7Lhk+hnRO8YLpMwX4O3p29suBjuC0Q3GE8AprBKqo4QAPnaBbAu9NMnUa6U02MjPUlN9+AOsG5LhLUSxTUe61pG2+/AL+j9tNbLON5AHgbK/0+eAX3AfgDFgWzFBBk3XB2A4KdQQSXAcFFFD6Cx4DgCaMRwVwBpIW1NK2C8+xO39n49LdPMJk7n8AxXnO99YhgK126Sl+2Vyqpk3U3I4Jsn7N97jXS/YP5AB/luaLoTDvsAAAAAElFTkSuQmCC',
 }
 
+export const mockSentenceLongEnglish =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 export const mockChats = [
   {
     message: 'こんにちは',
@@ -27,8 +29,7 @@ export const mockChats = [
     user: mockUser2,
   },
   {
-    message:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    message: mockSentenceLongEnglish,
     user: mockUser3,
   },
   {
