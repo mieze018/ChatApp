613f1830c5d788cd2475ffe30cb96e05f45b6176
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-16 17:49:22 +0900

    🧰  Storybookの設定
    tailwindCSSやtwin.macro、svgコンポーネントがコンパイルできるようにしている

---
+++ .storybook/main.ts
@@ -0,0 +1,39 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+import { svgrWebpackConfig } from '../svgr.webpack.config'
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+        // For more details on this addon's options.
+        postCss: true,
+      },
+    },
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: { fastRefresh: true },
+  },
+  webpackFinal: (config) => {
+    // Default rule for images /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test && rule.test.test('.svg'))
+    fileLoaderRule.exclude = /\.svg$/
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: 'pre',
+      loader: require.resolve('@svgr/webpack'),
+    })
+
+    return config
+  },
+  docs: {
+    autodocs: true,
+  },
+}
+export default config
