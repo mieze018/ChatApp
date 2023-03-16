1c732b7dd24f251796cb272995d880229909ad08
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-16 18:04:33 +0900

    ⛏   テキストをハードコーディングしないため・文言の差異を生まないための簡易辞書

---
+++ src/libs/microCopies.ts
@@ -0,0 +1,10 @@
+export const microCopies = {
+  signInHeader: 'チャットを始めましょう',
+  signInSubmit: 'チャットルームへ入室',
+  srAvatarInput: 'ユーザーアイコンを選択してください',
+  signInDisplayNamePlaceholder: 'ユーザー名',
+  chatBlank: '最初のチャットを投稿しましょう',
+  noName: 'No Name',
+  signUp: 'サインアップ',
+  signOut: 'サインアウト',
+}
