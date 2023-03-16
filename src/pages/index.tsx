42ec3064c12efac86bbfe0109fd2c8d1cffa8fb7
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-16 18:10:04 +0900

    ✨  indexでstate管理、表示画面の切り替え

---
+++ src/pages/index.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react'
+
+import type { User } from 'firebase/auth'
+import type { AppProps } from 'next/app'
+
+import { OverRayLoading } from '@/src/components/combined/OverRayLoading'
+import { LayoutChat } from '@/src/components/layout/LayoutChat'
+import { LayoutSignIn } from '@/src/components/layout/LayoutSignIn'
+import { useAuthDelete } from '@/src/hooks/firebase/useAuthDelete'
+import { useGetMessages } from '@/src/hooks/firebase/useGetMessages'
+import { usePostMessage } from '@/src/hooks/firebase/usePostMessage'
+import { useSignUp } from '@/src/hooks/firebase/useSingUp'
+import { authListener } from '@/src/libs/firebase/authListener'
+
+export default function Home() {
+  const [user, setUser] = useState<userStateType['user']>(undefined)
+  const [isAuthLoading, setIsAuthLoading] = useState(false)
+  const { displayName, setDisplayName, setFile, file, error, handleSignUp, progress } = useSignUp({
+    setUser,
+  })
+  useEffect(() => {
+    authListener({ setUser, setIsAuthLoading })
+  }, [user])
+  const { chats, isLoading, isBlank } = useGetMessages(user)
+  const { message, setMessage, handleSendMessage } = usePostMessage()
+  const { deleteAccount } = useAuthDelete({ setIsAuthLoading })
+  const isInitLoading = isAuthLoading || user === undefined
+  const isPhotoUploaded = user && user.photoURL
+  const progressing = progress !== undefined && progress >= 0
+  const isSubmitBlocked = !!(!displayName || !file || progressing)
+  const isPosting = progressing || isAuthLoading
+
+  /** 表示に必要な情報のみをPick */
+  const userToDisplay = user
+    ? {
+        displayName: user?.displayName,
+        photoURL: user?.photoURL,
+        uid: user?.uid,
+      }
+    : undefined
+
+  if (isInitLoading) return <OverRayLoading />
+  if (isPhotoUploaded) {
+    return (
+      <>
+        <LayoutChat
+          chats={chats}
+          isLoading={isLoading}
+          isBlank={isBlank}
+          message={message}
+          setMessage={setMessage}
+          handleSendMessage={handleSendMessage}
+          user={user}
+          onLogout={deleteAccount}
+          error={error}
+        />
+        {isPosting && <OverRayLoading progressPercentage={progress} />}
+      </>
+    )
+  }
+  return (
+    <>
+      <LayoutSignIn
+        user={userToDisplay}
+        onLogout={deleteAccount}
+        handleSignUp={handleSignUp}
+        setDisplayName={setDisplayName}
+        displayName={displayName}
+        setFile={setFile}
+        file={file}
+        error={error}
+        progress={progress}
+        isSubmitBlocked={isSubmitBlocked}
+      />
+      {isPosting && <OverRayLoading progressPercentage={progress} />}
+    </>
+  )
+}
+
+export type userStateType = {
+  user: User | null | undefined
+  setUser: React.Dispatch<React.SetStateAction<User | null | undefined>>
+}
+export type authLoadingStateType = {
+  isAuthLoading: boolean
+  setIsAuthLoading: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export type AppPropsType = userStateType & authLoadingStateType & AppProps
