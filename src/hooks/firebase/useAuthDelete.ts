c35415f34ac73de7bc95e7a47fe4861eeee9d92a
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-16 17:57:58 +0900

    ⛏   サインアウト(認証ユーザー削除)するhook
    セッションが切れると削除せずに再利用もできないユーザーとして残るが、匿名ユーザーは時間が経てば自動的に削除される

---
+++ src/hooks/firebase/useAuthDelete.ts
@@ -0,0 +1,30 @@
+import { FirebaseError } from 'firebase/app'
+import { getAuth, deleteUser } from 'firebase/auth'
+
+import type { authLoadingStateType } from '@/src/pages'
+
+import { useError } from '@/src/hooks/firebase/useError'
+
+export const useAuthDelete = ({
+  setIsAuthLoading,
+}: {
+  setIsAuthLoading: authLoadingStateType['setIsAuthLoading']
+}) => {
+  const { error, setError } = useError()
+  const deleteAccount = () => {
+    const auth = getAuth()
+    if (!auth.currentUser) return
+    setIsAuthLoading(true)
+    deleteUser(auth.currentUser)
+      .then(() => {
+        setIsAuthLoading(false)
+      })
+      .catch((e) => {
+        if (e instanceof FirebaseError) {
+          setError(e)
+        }
+      })
+  }
+  return { deleteAccount, error }
+}
+export type useAuthDeleteType = ReturnType<typeof useAuthDelete>
