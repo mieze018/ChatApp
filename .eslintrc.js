aab7ee27be3b3a581363b3b390df6af51879bba3
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-16 17:41:34 +0900

    🧰  ESLintルール

---
+++ .eslintrc.js
@@ -0,0 +1,85 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:css/recommended',
+    'next',
+    'next/core-web-vitals',
+    'prettier',
+    'plugin:storybook/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'css', 'unused-imports'],
+  root: true,
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'object', 'type'],
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    'import/no-duplicates': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'no-multi-spaces': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    // 未使用のインポートを自動で削除
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    // 相対パスのimportを禁止
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['./', '../'],
+      },
+    ],
+    // consoleを禁止
+    'no-console': 'error',
+    // 行間を開ける
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'export',
+      },
+      {
+        blankLine: 'any',
+        prev: 'export',
+        next: 'export',
+      },
+      {
+        blankLine: 'always',
+        prev: 'import',
+        next: '*',
+      },
+      {
+        blankLine: 'any',
+        prev: 'import',
+        next: 'import',
+      },
+    ],
+  },
+}
